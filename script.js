19d3129f5ac947892c71319df36fe0ab4c0efbcd
Paulo Miguel <150275885+Paulo-Mikhael@users.noreply.github.com>
2024-01-09 19:02:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,72 @@
 function menuShow(){
-    let menuMobile=document.querySelector('.mobile-menu')
-    if (menuMobile.classList.contains('open')){
-        menuMobile.classList.remove('open')
-        document.querySelector('.mobile-icon').src="img/x-preto.png"
-    }else{
+  let menuMobile=document.querySelector('.mobile-menu')
+  if (isLight)
+  {
+    if (menuMobile.classList.contains('open'))
+    {
+      menuMobile.classList.remove('open')
+      document.querySelector('.mobile-icon').src="img/x-preto.png"
+    }
+    else
+    {
       menuMobile.classList.add('open')
       document.querySelector('.mobile-icon').src="img/tres-barras-preto.png"
     }
+  }
+  else
+  {
+    if (menuMobile.classList.contains('open'))
+    {
+      menuMobile.classList.remove('open')
+      document.querySelector('.mobile-icon').src="img/x-branco.png"
+    }
+    else
+    {
+      menuMobile.classList.add('open')
+      document.querySelector('.mobile-icon').src="img/tres-barras-branco.png"
+    }
+  }
 }
 
 const toggle = document.querySelector("#toggle")
+const toggleMobile = document.querySelector(".moon-icon-mobile")
+const mobileMenu = document.querySelector('.mobile-menu')
 toggle.addEventListener("click", modeSwitch)
+toggleMobile.addEventListener("click", modeSwitch)
 let isLight=true
 function modeSwitch(){
-    isLight=!isLight
-    let root=document.body
-    if (isLight){
-      toggle.innerHTML="☀️";
-      document.querySelector(".perfil-image").src="img/moldura_perfil_preto.png";
+  isLight=!isLight
+  let root=document.body
+  if (isLight)
+  {
+    toggle.innerHTML="☀️";
+
+    if (mobileMenu.classList.contains('open'))
+    {
+      document.querySelector('.mobile-icon').src = "img/tres-barras-preto.png"
     }
-    else{
-      toggle.innerHTML="🌑";
-      document.querySelector(".perfil-image").src="img/moldura_perfil_branco.png";
+    else
+    {
+      document.querySelector('.mobile-icon').src="img/x-preto.png"
     }
-    root.classList.toggle("lightMode")
+
+    document.querySelector(".perfil-image").src="img/moldura_perfil_preto.png";
+  }
+  else
+  {
+    toggle.innerHTML="🌑";
+    
+    if (mobileMenu.classList.contains('open'))
+    {
+      document.querySelector('.mobile-icon').src="img/tres-barras-branco.png"
+    }
+    else
+    {
+      document.querySelector('.mobile-icon').src="img/x-branco.png"
+    }
+    
+
+    document.querySelector(".perfil-image").src="img/moldura_perfil_branco.png";
+  }
+  root.classList.toggle("lightMode")
 }
