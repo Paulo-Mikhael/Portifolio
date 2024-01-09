b25c426d639294ce278bd02a04982fc5a055d4a5
Paulo Miguel <150275885+Paulo-Mikhael@users.noreply.github.com>
2024-01-09 08:40:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function menuShow(){
       document.querySelector('.mobile-icon').src="img/tres-barras-preto.png"
     }
 }
+
+const toggle = document.querySelector("#toggle")
+toggle.addEventListener("click", modeSwitch)
+let isLight=true
+function modeSwitch(){
+    isLight=!isLight
+    let root=document.body
+    if (isLight){
+      toggle.innerHTML="☀️";
+      document.querySelector(".perfil-image").src="img/moldura_perfil_preto.png";
+    }
+    else{
+      toggle.innerHTML="🌑";
+      document.querySelector(".perfil-image").src="img/moldura_perfil_branco.png";
+    }
+    root.classList.toggle("lightMode")
+}
