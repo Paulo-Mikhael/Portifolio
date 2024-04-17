7df1ae2ae07f077f9997d35dd265ef53a24f81c8
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-04-16 20:01:48 -0400

    Mudando o novo layout para a main

---
+++ src/javascript/script.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal( { reset: true } );
+
+sr.reveal('#card-front-end', { 
+    duration: 2000,
+    scale: 0.2,
+});
+sr.reveal('#card-software', { 
+    duration: 2000,
+    scale: 0.2,
+});
+sr.reveal('#card-back-end', { 
+    duration: 2000,
+    scale: 0.2,
+});
+sr.reveal('.services-title', { 
+    duration: 3000,
+});
