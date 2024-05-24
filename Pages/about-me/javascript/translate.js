981281f7fb413f5338cd81734857110c5264d726
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-05-24 11:50:14 -0400

    Version 2 done

---
+++ Pages/about-me/javascript/translate.js
@@ -0,0 +1,15 @@
+//Variables
+const flagImage = document.querySelector('#header-image');
+const aboutMeSubtitle = document.querySelector('about-me-letter-spacing');
+const aboutMeTitle = document.querySelector('about-me-title');
+const aboutMeText = document.querySelector('about-me-text');
+
+//Functions
+function translate(){
+    
+}
+
+//Events
+flagImage.addEventListener('click', () => {
+
+});
